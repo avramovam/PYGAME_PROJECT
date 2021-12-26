02058e9831074f58f246a6179348feff34014cf2
Alexey <NotLexa>
2021-12-26 21:54:20 +0300

    Добавлена возможность изменять размер окна, холст Screen подгоняется безошибочно под измененный размер.

--- engine.py
+++ engine.py
@@ -3,6 +3,7 @@ import sys
 from ctypes import windll
 
 from typing import List, Tuple, Union
+from math import sin, cos, radians, degrees, floor, ceil
 
 import pygame
 
@@ -10,7 +11,7 @@ import pygame
 SCREENSIZE = windll.user32.GetSystemMetrics(0), windll.user32.GetSystemMetrics(1)
 
 
-def clamp(value: int, mn: int, mx: int):
+def clamp(value: int, mn: int, mx: int) -> int:
     return min(mx, max(mn, value))
 
 
@@ -41,6 +42,7 @@ class Entity:
         new_instance = Instance(entity = self)
         self.instances.append(new_instance)
         return new_instance
+
 
 class Instance:
     def __init__(self, entity: Entity):
@@ -75,6 +77,7 @@ class Instance:
     def do_user(self, index: int):
         self.entity.event_user[index](target=self)
 
+
 class EntityGroup:
     def __init__(self, entities: List[Entity] = None):
         if entities is None:
@@ -93,6 +96,7 @@ class EntityGroup:
         for ins in running: ins.do_step_after()
         for ins in running: ins.do_alerts()
 
+
 class Screen:
     '''Класс окна. В нем есть холст (на который наносятся нарисованные объекты) и дисплей (то, что показывается).
        Есть возможность создания полноэкранного режима.
@@ -101,46 +105,87 @@ class Screen:
        | 0 - оконный режим
        | 1 - полноэкранный режим
        | 2 - смешанный режим (оконнный на весь экран)'''
-    def __init__(self, canvas_size: Tuple[int, int], realscreen_size: Tuple[int, int], fullscreen_mode: int = 0):
+    def __init__(self, canvas_size: Tuple[int, int], realscreen_size: Tuple[int, int], fullscreen_mode: int = 0, resizable_mode: bool = True):
         self.cs = self.csw, self.csh = canvas_size
         self.ss = self.ssw, self.ssh = realscreen_size
         self.canvas = pygame.Surface(self.cs)
-        self.fm = fullscreen_mode
-
+        self.fm = clamp(fullscreen_mode, 0, 2)
+        self.rm = bool(resizable_mode)
         if self.fm == 1:
             self.screen = pygame.display.set_mode(self.ss, pygame.FULLSCREEN)
         elif self.fm == 2:
             self.screen = pygame.display.set_mode(SCREENSIZE, pygame.NOFRAME)
         else:
-            self.screen = pygame.display.set_mode(self.ss)
+            self.screen = pygame.display.set_mode(self.ss, (self.rm * pygame.RESIZABLE))
 
-    def get_canvas(self):
+    def get_canvas(self) -> pygame.Surface:
         return self.canvas
 
-    def get_screen(self):
+    def get_screen(self) -> pygame.Surface:
         return self.screen
 
-    def update_screen(self, size: Tuple[int, int] = None, fullscreen_mode: bool = False):
+    def get_fullscreen(self) -> int:
+        return self.fm
+
+    def get_resizable(self) -> bool:
+        return self.rm
+
+    def get_canvas_size(self) -> Tuple[int, int]:
+        return self.cs
+
+    def get_screen_size(self) -> Tuple[int, int]:
+        return self.ss
+
+    def update_screen(self, size: Tuple[int, int] = None, fullscreen_mode: int = None, resizable_mode: bool = None):
+        '''Обновить данные экрана. Значение None в аргументах означает сохранение предыдущего значения.'''
         if size is None:
             size = self.ss
+        if fullscreen_mode is None:
+            fullscreen_mode = self.fm
+        if resizable_mode is None:
+            resizable_mode = self.rm
 
-        self.fm = fullscreen_mode
+        self.ss = self.ssw, self.ssh = size
+
+        self.fm = clamp(fullscreen_mode, 0, 2)
+        self.rm = bool(resizable_mode)
         if self.fm == 1:
             self.screen = pygame.display.set_mode(self.ss, pygame.FULLSCREEN)
         elif self.fm == 2:
             self.screen = pygame.display.set_mode(SCREENSIZE, pygame.NOFRAME)
         else:
-            self.screen = pygame.display.set_mode(self.ss)
+            self.screen = pygame.display.set_mode(self.ss, (self.rm * pygame.RESIZABLE))
+
+    def draw_screen(self):
+        scale_level = min(self.ssw/self.csw, self.ssh/self.csh)
+        scaled_size = (self.csw * scale_level, self.csh * scale_level)
+        delta_width = self.ssw - scaled_size[0]
+        delta_height = self.ssh - scaled_size[1]
+        self.screen.fill('black')
+        self.screen.blit(pygame.transform.scale(self.canvas, scaled_size), (delta_width//2, delta_height//2))
+
 
 if __name__ == '__main__':
+    print('TEST RUN')
     print(f'SCREENSIZE is: {SCREENSIZE[0]}, {SCREENSIZE[1]}')
 
-    s = Screen((200, 200), (400, 400), 2)
+    canvas_w = 200
+    canvas_h = 100
+    pixel_size = 2
+    canvas_size = (canvas_w, canvas_h)
+    screen_size = (canvas_w*pixel_size, canvas_h*pixel_size)
+    s = Screen(canvas_size, screen_size, 0, True)
 
     running = 1
     while running:
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 running = 0
+            if e.type == pygame.VIDEORESIZE:
+                s.update_screen((e.w, e.h))
+
+        pygame.draw.rect(s.get_canvas(), 'yellow', (0,0, *canvas_size), 4)
+        pygame.draw.line(s.canvas, 'white', (0,0), canvas_size, 2)
+        s.draw_screen()
 
         pygame.display.flip()
