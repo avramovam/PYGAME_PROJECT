e501d7b481c6b61933d00243610c518ffbf65a2f
Alexey <notlexa>
2022-01-03 19:18:02 +0300

    DVLP BUILD #3: 1) простой переход вникуда при нажатии мышкой по кнопке "Начать игру" 2) небольшой фикс парочки штуковин

--- NEON_CONSTELLATION.py
+++ NEON_CONSTELLATION.py
@@ -12,8 +12,8 @@ print(''':P
                                             (Neon Constellation)
 by:                                                                                      version:
     Alexey Kozhanov                                                                               DVLP BUILD
-    Andrey Avramov                                                                                        #2
-    Daria Stolyarova                                                                              02.01.2022
+    Andrey Avramov                                                                                        #3
+    Daria Stolyarova                                                                              03.01.2022
 ''')
 
 pygame.init()
@@ -53,9 +53,15 @@ def BG_create(target):
     target.image = None
     target.angle = 0
     target.offset = 8
+    target.gotofield = 0
+    target.gotofield_step = 0
 
 def BG_step(target):
     target.angle = (target.angle+UPF(30)) % 360
+    if target.gotofield:
+        target.gotofield_step += UPF(1/2) # полный переход за 2 секунды
+    if target.gotofield_step >= 1:
+        engine.rooms.change_current_room(room_field)
 
 def BG_draw_before(target, surface: pygame.Surface):
     #pygame.draw.circle(surface, 'yellow', (2, 2), 5.0)
@@ -65,10 +71,23 @@ def BG_draw_before(target, surface: pygame.Surface):
     height = surface.get_height()
     for x in ((-1, 0),(0, 1))[ox<0]:     # этот цикл отрисовывает картинку четырежды, чтобы в любом случае
         for y in ((-1, 0),(0, 1))[oy<0]: # замостить полностью холст. ((-1, 0),(0, 1))[ox<0] лишь оптимизирует это
-            surface.blit(target.image, (ox + (x*width),                                                  # - леша
-                                        oy + (y*height)))
+            surface.blit(target.image, (ox + (x * width),  # - леша
+                                        oy + (y * height)))
 
-EntBG = engine.Entity(event_create=BG_create, event_step=BG_step, event_draw_before=BG_draw_before)
+def BG_draw_after(target, surface: pygame.Surface):
+    '''Здесь будет рисоваться типо круг перехода из уровня в уровень'''
+    width = surface.get_width()
+    height = surface.get_height()
+    half_diagonal = (((width**2) + (height**2))**0.5) / 2
+    circle_surface = pygame.Surface(surface.get_size(), pygame.SRCALPHA)
+    circle_surface2 = pygame.Surface(surface.get_size(), pygame.SRCALPHA)
+    circle_surface.fill('black')
+    pygame.draw.circle(circle_surface2, 'black', (width/2, height/2), half_diagonal * (1 - target.gotofield_step), 0)
+    circle_surface.blit(circle_surface2, (0, 0), None, pygame.BLEND_RGBA_SUB)
+    surface.blit(circle_surface, (0, 0), None)
+
+EntBG = engine.Entity(event_create=BG_create, event_step=BG_step, event_draw_before=BG_draw_before,
+                      event_draw_after=BG_draw_after)
 #endregion
 #region [MAINMENU TEXT]
 def mm_text_create(target):
@@ -80,7 +99,7 @@ def mm_text_create(target):
     instance_render_text(target)
 
 
-def mm_text_draw_after(target, surface: pygame.Surface):
+def mm_text_draw(target, surface: pygame.Surface):
     lineoffset = 0
     maxtw = max([x.get_width() for x in target.text])
     for lineindex in range(len(target.text)):
@@ -94,15 +113,19 @@ def mm_text_draw_after(target, surface: pygame.Surface):
         else:  # if target.text_align == 1:
             surface.blit(target.text[lineindex], (target.x - (tw//2), target.y - (th//2) + lineoffset))
 
-EntMainMenuText = engine.Entity(event_create=mm_text_create, event_draw_after=mm_text_draw_after)
+EntMainMenuText = engine.Entity(event_create=mm_text_create, event_draw=mm_text_draw)
 #endregion
 #region [MAINMENU BUTTON]
+def mm_button_user0(): # Переход на поле
+    bg1.gotofield = 1
+
 def mm_button_create(target):
     target.x, target.y = 0, 0
     target.font = font_default
     target.string = 'Sample Text'
     target.text_color = 'black'
     instance_render_text(target)
+    target.press_link = None
 
 def mm_button_draw(target, surface: pygame.Surface):
     tw = target.text[0].get_width()
@@ -116,13 +139,26 @@ def mm_button_draw(target, surface: pygame.Surface):
     pygame.draw.rect(surface, 'gray',  rect_coords, 3, *full_rounding) # обводка задника
     surface.blit(target.text[0], text_coords)
 
+def mm_button_mouse_pressed(target, mousepos, buttonid):
+    tw = target.text[0].get_width()
+    th = target.text[0].get_height()
+    fs = 16  # размер поля между прямоугольником и текстом
+    xrange = target.x - ((tw+fs)//2) <= mousepos[0] <= target.x + ((tw+fs)//2)
+    yrange = target.y - ((th+fs)//2) <= mousepos[1] <= target.y + ((th+fs)//2)
+    if xrange and yrange:
+        if target.press_link is not None:
+            target.press_link()
+
 # EntMainMenuButton копирует метод создания и рендера текста из EntGameTitle
-EntMainMenuButton = engine.Entity(event_create=mm_button_create, event_draw=mm_button_draw)
+EntMainMenuButton = engine.Entity(event_create=mm_button_create, event_draw=mm_button_draw,
+                                  event_mouse_pressed=mm_button_mouse_pressed)
 #endregion
 #endregion
 
 #region [ОБЪЯВЛЕНИЕ ROOM]
 room_mainmenu = engine.Room([EntBG, EntMainMenuText, EntMainMenuButton])
+
+room_field = engine.Room([])
 
 engine.rooms.change_current_room(room_mainmenu)
 #endregion
@@ -151,18 +187,19 @@ mmb1.x = screen.get_canvas_halfwidth()
 mmb1.y = screen.get_canvas_halfheight() - 48
 mmb1.string = 'Начать игру'
 instance_render_text(mmb1)
+mmb1.press_link = mm_button_user0
 
-mmb1 = EntMainMenuButton.instance()
-mmb1.x = screen.get_canvas_halfwidth()
-mmb1.y = screen.get_canvas_halfheight()
-mmb1.string = 'Настройки'
-instance_render_text(mmb1)
+mmb2 = EntMainMenuButton.instance()
+mmb2.x = screen.get_canvas_halfwidth()
+mmb2.y = screen.get_canvas_halfheight()
+mmb2.string = 'Настройки'
+instance_render_text(mmb2)
 
-mmb1 = EntMainMenuButton.instance()
-mmb1.x = screen.get_canvas_halfwidth()
-mmb1.y = screen.get_canvas_halfheight() + 48
-mmb1.string = 'Выйти'
-instance_render_text(mmb1)
+mmb3 = EntMainMenuButton.instance()
+mmb3.x = screen.get_canvas_halfwidth()
+mmb3.y = screen.get_canvas_halfheight() + 48
+mmb3.string = 'Выйти'
+instance_render_text(mmb3)
 #endregion
 
 #region [КОНСТАНТЫ, ПЕРЕМЕННЫЕ И Т.Д.]
@@ -179,6 +216,16 @@ while game_running:
             game_running = 0
         elif event.type == pygame.VIDEORESIZE: # изменение размера экрана
             screen.update_screen((event.w, event.h))
+        elif event.type == pygame.MOUSEMOTION:
+            engine.rooms.current_room.do_mouse_moved(screen.get_mousepos_on_canvas(event.pos))
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            engine.rooms.current_room.do_mouse_pressed(screen.get_mousepos_on_canvas(event.pos), event.button)
+        elif event.type == pygame.MOUSEBUTTONUP:
+            engine.rooms.current_room.do_mouse_released(screen.get_mousepos_on_canvas(event.pos), event.button)
+        elif event.type == pygame.KEYDOWN:
+            engine.rooms.current_room.do_kb_pressed(event.key)
+        elif event.type == pygame.KEYUP:
+            engine.rooms.current_room.do_kb_released(event.key)
 
 
     screen.get_canvas().fill('black')
