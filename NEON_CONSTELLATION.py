0b3e7c9d42d1d27e956dda7737eea2772536551b
Alexey <notlexa>
2022-01-06 22:34:29 +0300

    DVLP BUILD #4: Сетка

--- NEON_CONSTELLATION.py
+++ NEON_CONSTELLATION.py
@@ -1,5 +1,6 @@
 import pygame
 import engine
+from math import floor, ceil
 
 #region [ИНИЦИАЛИЗАЦИЯ ПРОГРАММЫ]
 print(''':P
@@ -12,8 +13,8 @@ print(''':P
                                             (Neon Constellation)
 by:                                                                                      version:
     Alexey Kozhanov                                                                               DVLP BUILD
-    Andrey Avramov                                                                                        #3
-    Daria Stolyarova                                                                              03.01.2022
+    Andrey Avramov                                                                                        #4
+    Daria Stolyarova                                                                              06.01.2022
 ''')
 
 pygame.init()
@@ -48,22 +49,22 @@ def instance_render_text(target):
 #endregion
 
 #region [ОБЪЯВЛЕНИЕ ENTITY]
-#region [BG]
-def BG_create(target):
+#region [MAINMENU BG]
+def MainMenuBG_create(target):
     target.image = None
     target.angle = 0
     target.offset = 8
     target.gotofield = 0
     target.gotofield_step = 0
 
-def BG_step(target):
+def MainMenuBG_step(target):
     target.angle = (target.angle+UPF(30)) % 360
     if target.gotofield:
         target.gotofield_step += UPF(1/2) # полный переход за 2 секунды
     if target.gotofield_step >= 1:
         engine.rooms.change_current_room(room_field)
 
-def BG_draw_before(target, surface: pygame.Surface):
+def MainMenuBG_draw_before(target, surface: pygame.Surface):
     #pygame.draw.circle(surface, 'yellow', (2, 2), 5.0)
     ox = engine.lengthdir_x(target.offset, target.angle)
     oy = engine.lengthdir_y(target.offset, target.angle)
@@ -74,7 +75,7 @@ def BG_draw_before(target, surface: pygame.Surface):
             surface.blit(target.image, (ox + (x * width),  # - леша
                                         oy + (y * height)))
 
-def BG_draw_after(target, surface: pygame.Surface):
+def MainMenuBG_draw_after(target, surface: pygame.Surface):
     '''Здесь будет рисоваться типо круг перехода из уровня в уровень'''
     width = surface.get_width()
     height = surface.get_height()
@@ -82,15 +83,15 @@ def BG_draw_after(target, surface: pygame.Surface):
     circle_surface = pygame.Surface(surface.get_size(), pygame.SRCALPHA)
     circle_surface2 = pygame.Surface(surface.get_size(), pygame.SRCALPHA)
     circle_surface.fill('black')
-    pygame.draw.circle(circle_surface2, 'black', (width/2, height/2), half_diagonal * (1 - target.gotofield_step), 0)
+    pygame.draw.circle(circle_surface2, 'black', (width/2, height/2), half_diagonal * (max(0, 1.4*(1 - target.gotofield_step) - 0.4)), 0)
     circle_surface.blit(circle_surface2, (0, 0), None, pygame.BLEND_RGBA_SUB)
     surface.blit(circle_surface, (0, 0), None)
 
-EntBG = engine.Entity(event_create=BG_create, event_step=BG_step, event_draw_before=BG_draw_before,
-                      event_draw_after=BG_draw_after)
+EntMainMenuBG = engine.Entity(event_create=MainMenuBG_create, event_step=MainMenuBG_step,
+                              event_draw_before=MainMenuBG_draw_before, event_draw_after=MainMenuBG_draw_after)
 #endregion
 #region [MAINMENU TEXT]
-def mm_text_create(target):
+def MainMenuText_create(target):
     target.x, target.y = 0, 0
     target.font = font_default
     target.string = 'Neon Constellation'
@@ -99,7 +100,7 @@ def mm_text_create(target):
     instance_render_text(target)
 
 
-def mm_text_draw(target, surface: pygame.Surface):
+def MainMenuText_draw(target, surface: pygame.Surface):
     lineoffset = 0
     maxtw = max([x.get_width() for x in target.text])
     for lineindex in range(len(target.text)):
@@ -113,13 +114,13 @@ def mm_text_draw(target, surface: pygame.Surface):
         else:  # if target.text_align == 1:
             surface.blit(target.text[lineindex], (target.x - (tw//2), target.y - (th//2) + lineoffset))
 
-EntMainMenuText = engine.Entity(event_create=mm_text_create, event_draw=mm_text_draw)
+EntMainMenuText = engine.Entity(event_create=MainMenuText_create, event_draw=MainMenuText_draw)
 #endregion
 #region [MAINMENU BUTTON]
-def mm_button_user0(): # Переход на поле
+def MainMenuButton_user0(): # Переход на поле
     bg1.gotofield = 1
 
-def mm_button_create(target):
+def MainMenuButton_create(target):
     target.x, target.y = 0, 0
     target.font = font_default
     target.string = 'Sample Text'
@@ -127,7 +128,7 @@ def mm_button_create(target):
     instance_render_text(target)
     target.press_link = None
 
-def mm_button_draw(target, surface: pygame.Surface):
+def MainMenuButton_draw(target, surface: pygame.Surface):
     tw = target.text[0].get_width()
     th = target.text[0].get_height()
     fs = 16 # размер поля между прямоугольником и текстом
@@ -139,7 +140,7 @@ def mm_button_draw(target, surface: pygame.Surface):
     pygame.draw.rect(surface, 'gray',  rect_coords, 3, *full_rounding) # обводка задника
     surface.blit(target.text[0], text_coords)
 
-def mm_button_mouse_pressed(target, mousepos, buttonid):
+def MainMenuButton_mouse_pressed(target, mousepos, buttonid):
     tw = target.text[0].get_width()
     th = target.text[0].get_height()
     fs = 16  # размер поля между прямоугольником и текстом
@@ -150,21 +151,90 @@ def mm_button_mouse_pressed(target, mousepos, buttonid):
             target.press_link()
 
 # EntMainMenuButton копирует метод создания и рендера текста из EntGameTitle
-EntMainMenuButton = engine.Entity(event_create=mm_button_create, event_draw=mm_button_draw,
-                                  event_mouse_pressed=mm_button_mouse_pressed)
+EntMainMenuButton = engine.Entity(event_create=MainMenuButton_create, event_draw=MainMenuButton_draw,
+                                  event_mouse_pressed=MainMenuButton_mouse_pressed)
+#endregion
+#region [FIELD BG]
+def FieldBG_create(target):
+    target.image = None
+    target.angle = 0
+    target.offset = 8
+    target.fadeout = 1
+
+def FieldBG_draw_before(target, surface):
+    ox = 0
+    oy = 0
+    width = surface.get_width()
+    height = surface.get_height()
+    surface.blit(target.image, (ox + (target.x * width), oy + (target.y * height)))
+
+EntFieldBG = engine.Entity(event_create=FieldBG_create, event_draw_before=FieldBG_draw_before)
+#endregion
+
+#region [FIELD BOARD]
+def FieldBoard_user0(target, surface: pygame.Surface):
+    '''Расчет start_x и start_y согласно центрированию на surface'''
+    surface_center_x = surface.get_width()//2
+    surface_center_y = surface.get_height()//2
+    target.start_x = surface_center_x - (target.width//2)
+    target.start_y = surface_center_y - (target.height//2)
+
+def FieldBoard_create(target):
+    target.cellsize = 18
+    target.cellcount_x = 10
+    target.cellcount_y = 10
+    # target.start_x = 0
+    # target.start_y = 0
+    target.width = ((target.cellsize+1) * target.cellcount_x) + 1
+    target.height = ((target.cellsize+1) * target.cellcount_y) + 1
+    FieldBoard_user0(target, screen.get_canvas())
+    target.boardalpha = 1 # уровень прозрачности от 0 (полностью прозрачный) до 1 (полностью видимый)
+
+
+def FieldBoard_draw_before(target, surface: pygame.Surface):
+    board_surface = pygame.Surface((target.width, target.height), pygame.SRCALPHA)
+    board_surface.fill((0,0,0,0))
+    # попиксельное отрисовывание сетки хехехехехехехехехе ужас
+    cellsize_with_border = target.cellsize + 1
+    for px in range(target.width):
+        for py in range(target.height):
+            if py % cellsize_with_border == 0: # линия по горизонтали
+                minimal = floor(px / cellsize_with_border) * cellsize_with_border # близжайшее левое пересечение линий
+                maximal = ceil(px / cellsize_with_border) * cellsize_with_border # близжайшее правое пересечение линий
+
+                if minimal == maximal: # мы находимся на пересечении
+                    alpha = 1
+                else:
+                    alpha = max(abs(minimal-px), abs(maximal-px)) / cellsize_with_border
+                    alpha = 2 * (alpha - 0.5)
+                board_surface.set_at((px, py), (255, 255, 255, 255 * alpha * target.boardalpha))
+            elif px % cellsize_with_border == 0:  # линия по горизонтали
+                minimal = floor(py / cellsize_with_border) * cellsize_with_border  # близжайшее верхнее пересечение линий
+                maximal = ceil(py / cellsize_with_border) * cellsize_with_border  # близжайшее нижнее пересечение линий
+
+                if minimal == maximal:  # мы находимся на пересечении
+                    alpha = 1
+                else:
+                    alpha = max(abs(minimal - py), abs(maximal - py)) / cellsize_with_border
+                    alpha = 2 * (alpha - 0.5)
+                board_surface.set_at((px, py), (255, 255, 255, 255 * alpha * target.boardalpha))
+    surface.blit(board_surface, (target.start_x, target.start_y))
+
+
+EntFieldBoard = engine.Entity(event_create=FieldBoard_create, event_draw_before=FieldBoard_draw_before)
 #endregion
 #endregion
 
 #region [ОБЪЯВЛЕНИЕ ROOM]
-room_mainmenu = engine.Room([EntBG, EntMainMenuText, EntMainMenuButton])
+room_mainmenu = engine.Room([EntMainMenuBG, EntMainMenuText, EntMainMenuButton])
 
-room_field = engine.Room([])
+room_field = engine.Room([EntFieldBG, EntFieldBoard])
 
 engine.rooms.change_current_room(room_mainmenu)
 #endregion
 
 #region [СОЗДАНИЕ INSTANCE]
-bg1 = EntBG.instance()
+bg1 = EntMainMenuBG.instance()
 bg1.image = img_bg
 
 gametitle = EntMainMenuText.instance()
@@ -187,7 +257,7 @@ mmb1.x = screen.get_canvas_halfwidth()
 mmb1.y = screen.get_canvas_halfheight() - 48
 mmb1.string = 'Начать игру'
 instance_render_text(mmb1)
-mmb1.press_link = mm_button_user0
+mmb1.press_link = MainMenuButton_user0
 
 mmb2 = EntMainMenuButton.instance()
 mmb2.x = screen.get_canvas_halfwidth()
@@ -200,6 +270,8 @@ mmb3.x = screen.get_canvas_halfwidth()
 mmb3.y = screen.get_canvas_halfheight() + 48
 mmb3.string = 'Выйти'
 instance_render_text(mmb3)
+
+fboard = EntFieldBoard.instance()
 #endregion
 
 #region [КОНСТАНТЫ, ПЕРЕМЕННЫЕ И Т.Д.]
